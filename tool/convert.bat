10e4bf1c9d5a7826279ebbd6ff231439926598b6
kelvin7feng <kelvin7.feng@gmail.com>
2020-04-25 17:02:08 +0800

    修改共享代码

--- tool/convert.bat
+++ tool/convert.bat
@@ -1,5 +1,4 @@
 python convert.py
-xcopy ..\client ..\..\rpg\Scripts\Lua\share\config /Y/E
-xcopy ..\client ..\..\1st_server\script\share\config /Y/E
+xcopy ..\client ..\..\share\config /Y/E
 
 pause
