9b42413c3b4c8c4ed92d80c07b1ec9665f3b24be
kelvin7feng <kelvin7.feng@gmail.com>
2020-06-15 22:30:09 +0800

    1.修改成就 2.修改家升级配置

--- client/redirectPage.lua
+++ client/redirectPage.lua
@@ -6,4 +6,18 @@ _cfg.redirectPage = {
    redirectParam2 = '',
    redirectParam3 = '',
   },
+  [2] = {
+   id = 2,
+   redirect = 'game/house/pkgUIHouse',
+   redirectParam1 = '',
+   redirectParam2 = '',
+   redirectParam3 = '',
+  },
+  [3] = {
+   id = 3,
+   redirect = 'game/cropland/pkgUICropland',
+   redirectParam1 = '',
+   redirectParam2 = '',
+   redirectParam3 = '',
+  },
 }
