b302e18e4eddac1fdd4da1a5719997ed03707e17
kelvin7feng <kelvin7.feng@gmail.com>
2020-06-15 20:51:17 +0800

    特效配置

--- client/effect.lua
+++ client/effect.lua
@@ -8,19 +8,31 @@ _cfg.effect = {
   [2] = {
    id = 2,
    bundleName = 'particle',
-   assetName = 'FireballExplode',
+   assetName = 'HouseLevelUp',
    duration = 0,
   },
   [3] = {
    id = 3,
    bundleName = 'particle',
-   assetName = 'OuterSpaceExplode',
+   assetName = 'FireballExplode',
    duration = 0,
   },
   [4] = {
    id = 4,
    bundleName = 'particle',
-   assetName = 'BlueSpell1',
+   assetName = 'CircleBtnEffect',
+   duration = 0,
+  },
+  [5] = {
+   id = 5,
+   bundleName = 'particle',
+   assetName = 'FootCircle',
+   duration = 0,
+  },
+  [6] = {
+   id = 6,
+   bundleName = 'particle',
+   assetName = 'CircleBtnEffect',
    duration = 0,
   },
 }
